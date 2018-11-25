e4b9f79562dd8c53879a7dbe10ec3db5e997c60e
Ed <edward.dobson3@ntlworld.com>
2018-11-25 16:27:30 +0000

    Please work

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-#Get short commit hash from this repo
 HASH=$(git rev-parse --short HEAD)
-BODY=$(printf '{"tag_name": %s", "target_commitish": "master", "name": "%s", "draft": false, "prerelease": false}' ${HASH} ${HASH})
 
-#Create new release in release repo
-curl --data ${BODY} https://api.github.com/repos/GodlikePenguin/agogos-host-release/releases?access_token=$GITHUB_API_TOKEN
+go get github.com/aktau/github-release
+
+github-release release \
+  --user GodlikePenguin \
+  --repo agogos-host-release \
+  --tag ${HASH} \
+  --name ${HASH} \
+  --description "Release for tag ${HASH}"
+
+github-release upload \
+  --user GodlikePenguin \
+  --repo agogos-host-release \
+  --tag ${HASH} \
+  --name "agogos-host-darwin" \
+  --file build/agogos-host-darwin
+
+github-release upload \
+  --user GodlikePenguin \
+  --repo agogos-host-release \
+  --tag ${HASH} \
+  --name "agogos-host-linux" \
+  --file build/agogos-host-linux
