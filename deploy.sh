5368ad7cdc57c0c6548b4fbb7a8fff9ea8e7597d
Ed <edward.dobson3@ntlworld.com>
2018-11-25 16:09:09 +0000

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 #Get short commit hash from this repo
-HASH=git rev-parse --short HEAD
+HASH=$(git rev-parse --short HEAD)
+BODY=$(printf '{"tag_name": %s", "target_commitish": "master", "name": "%s", "draft": false, "prerelease": false}' ${HASH} ${HASH})
 
 #Create new release in release repo
-curl --data '{"tag_name": "$HASH",
-              "target_commitish": "master",
-              "name": "$HASH",
-              "draft": false,
-              "prerelease": false}' \
-    https://api.github.com/repos/GodlikePenguin/agogos-host-release/releases?access_token=$GITHUB_API_TOKEN
+curl --data ${BODY} https://api.github.com/repos/GodlikePenguin/agogos-host-release/releases?access_token=$GITHUB_API_TOKEN
